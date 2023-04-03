9180f451c1535e0e3093e72e5bbb70ab658a5b86
Xinhang (Owen) Ma <89664309+xhOwenMa@users.noreply.github.com>
2023-04-03 11:38:25 -0500

    Update prime_test.py

--- primality_test/prime_test.py
+++ primality_test/prime_test.py
@@ -1,15 +1,42 @@
-def is_prime(i):
+def is_prime(n):
   """
-  test if i is prime
-  
-  i : int
+  n : int
+  return True if n is prime
   """
   
-    if i < 2:
+    if n < 2:
         return False
     
-    for d in range(2, int(i ** 0.5) + 1):
-        if i % d == 0:
+    for d in range(2, int(n ** 0.5) + 1):
+        if n % d == 0:
             return False
         
     return True
+
+def is_prime2(n):
+  """
+  fact: besides 2 and 3, all primes have the form 6k+1 or 6k-1. So this algorithm only looks at numbers of this form
+  
+  n : int
+  return True if n is prime
+  """
+  if n == 2:
+    return True
+  if n == 3:
+    return True
+  if n % 2 == 0:
+    return False
+  if n % 3 == 0:
+    return False
+
+  i = 5
+  w = 2
+
+  while i * i <= n:
+    if n % i == 0:
+      return False
+
+    i += w
+    w = 6 - w
+
+  return True
