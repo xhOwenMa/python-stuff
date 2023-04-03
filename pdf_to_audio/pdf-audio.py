990b5a88b2baca63b108fb7964ea09bfbc4ad96b
Xinhang (Owen) Ma <89664309+xhOwenMa@users.noreply.github.com>
2023-04-02 20:54:58 -0500

    Add files via upload

---
+++ pdf_to_audio/pdf-audio.py
@@ -0,0 +1,19 @@
+from winreg import HKEY_LOCAL_MACHINE
+import pyttsx3, PyPDF2
+
+pdfreader = PyPDF2.PdfReader(open('book.pdf', 'rb'))
+speaker = pyttsx3.init()
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[1].id) # set the voice to Zira - one of the Microsoft built-in voices
+# use voice.py the find the voice you like
+
+for page_num in range(len(pdfreader.pages)):
+    text = pdfreader.pages[page_num].extract_text()
+    clean_text = text.strip().replace('\n', ' ')
+    print(clean_text)
+    
+speaker.save_to_file(clean_text, 'story.mp3')
+
+speaker.runAndWait()
+
+speaker.stop()
