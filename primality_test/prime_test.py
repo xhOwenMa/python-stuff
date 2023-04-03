dfcb4e929d65409afc32c3a7d453fc9d8eff72a6
Xinhang (Owen) Ma <89664309+xhOwenMa@users.noreply.github.com>
2023-04-03 11:48:13 -0500

    Update prime_test.py

--- primality_test/prime_test.py
+++ primality_test/prime_test.py
@@ -1,8 +1,8 @@
 def is_prime(n):
-  """
-  n : int
-  return True if n is prime
-  """
+    """
+    n : int
+    return True if n is prime
+    """
   
     if n < 2:
         return False
@@ -14,29 +14,29 @@ def is_prime(n):
     return True
 
 def is_prime2(n):
-  """
-  fact: besides 2 and 3, all primes have the form 6k+1 or 6k-1. So this algorithm only checks divisors of this form
-  
-  n : int
-  return True if n is prime
-  """
-  if n == 2:
+    """
+    fact: besides 2 and 3, all primes have the form 6k+1 or 6k-1. So this algorithm only checks divisors of this form
+
+    n : int
+    return True if n is prime
+    """
+    if n == 2:
+        return True
+    if n == 3:
+        return True
+    if n % 2 == 0:
+        return False
+    if n % 3 == 0:
+        return False
+
+    d = 5
+    w = 2
+
+    while d * d <= n:
+        if n % d == 0:
+            return False
+
+        d += w
+        w = 6 - w
+
     return True
-  if n == 3:
-    return True
-  if n % 2 == 0:
-    return False
-  if n % 3 == 0:
-    return False
-
-  d = 5
-  w = 2
-
-  while d * d <= n:
-    if n % d == 0:
-      return False
-
-    d += w
-    w = 6 - w
-
-  return True
